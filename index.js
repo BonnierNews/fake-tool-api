883675601aac92cc301dd7eaa7fd773853936614
Oscar Tholander <oscar.tholander@bonniernews.se>
2024-04-02 15:29:17 +0200

    implement sorting on one attribute and pagination

--- index.js
+++ index.js
@@ -496,7 +496,7 @@ function search(url, body) {
     const ofType = contentByType[typeName];
     const ids = Object.keys(ofType);
 
-    return ids.map((id) => {
+    const mapped = ids.map((id) => {
       const content = ofType[id];
       const hit = {
         type: typeName,
@@ -510,6 +510,30 @@ function search(url, body) {
 
       return hit;
     });
+
+    const from = body.from || 0;
+    let size = body.size || mapped.length;
+
+    if (body.from && body.size) {
+      size = body.from + body.size;
+    }
+
+    const sliced = mapped.slice(from, size);
+
+    if (body.sort && Array.isArray(body.sort) && body.sort.length > 0) {
+      sliced.sort((aObj, bObj) => {
+        const a = aObj[body.sort[0].by];
+        const b = bObj[body.sort[0].by];
+        if (typeof a === "string" && typeof b === "string") {
+          if (body.sort[0].order === "asc") {
+            return a.localeCompare(b);
+          }
+          return b.localeCompare(a);
+        }
+      });
+    }
+
+    return sliced;
   });
 
   if (body.q) {
